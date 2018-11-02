4371683b0ad2d7a32a284236777da4d34f5ad56e
Bruno Zell <bruno.zzell@gmail.com>
2018-11-02 01:33:36 +0100

    Added a sample project

---
+++ samples/WhatsAppParser.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace WhatsAppParser.Console
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var parser = new Parser("chat.txt");
+            var messages = parser.Messages().ToList();
+        }
+    }
+}
