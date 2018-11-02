6bdfdf182f0919f3fd14e4026ed46edc7b1893df
Bruno Zell <bruno.zzell@gmail.com>
2018-11-02 14:02:57 +0100

    Rename sample to avoid naming collision

--- samples/WhatsAppParser.Console/Program.cs
+++ samples/WhatsAppParser.Console/Program.cs
@@ -1,13 +1,20 @@
-﻿using System.Linq;
+﻿using System;
 
-namespace WhatsAppParser.Console
+namespace WhatsAppParser.Terminal
 {
     public class Program
     {
         private static void Main(string[] args)
         {
+            // Create a parser from an exported chat history
             var parser = new Parser("chat.txt");
-            var messages = parser.Messages().ToList();
+
+            // Enumerate all messages in that history file
+            foreach (var message in parser.Messages()) {
+                Console.WriteLine($"{message.Timestamp:yyyy-MM-dd HH:mm} {message.Sender}: {message.Content}");
+            }
+
+            Console.ReadKey();
         }
     }
 }
